f8267cfadfb9341350a5bc4f05b226038be05c60
Marcin <mariczne@gmail.com>
2020-03-16 09:43:50 +0100

    Updates

---
+++ precache-manifest.7ebb5d5d0c0f61bbf5ca5ba02760fc24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45168ee6f29613d863e3783da976dec2",
+    "url": "/react-shopping-cart/index.html"
+  },
+  {
+    "revision": "cb3d6308395c175a1ee8",
+    "url": "/react-shopping-cart/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "cb3d6308395c175a1ee8",
+    "url": "/react-shopping-cart/static/js/2.4221ed9b.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/react-shopping-cart/static/js/2.4221ed9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8686209b8e7f570636dd",
+    "url": "/react-shopping-cart/static/js/main.84377dee.chunk.js"
+  },
+  {
+    "revision": "8c3e24d15e5f12b1e9d7",
+    "url": "/react-shopping-cart/static/js/runtime-main.c8904998.js"
+  }
+]);
