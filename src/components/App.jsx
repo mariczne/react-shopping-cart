d360e3ced8e7e417f3b5de904e6d0fc1bd206e27
Marcin <mariczne@gmail.com>
2020-03-19 01:08:48 +0100

    Rewritten with Hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import TopBar from "./TopBar.jsx";
 import Products from "./Products/Products.jsx";
@@ -9,107 +9,89 @@ const API_URL = // bypassing CORS via proxy for now
 
 const DATA_STATES = { loading: "loading", loaded: "loaded" };
 
-export default class App extends Component {
-  state = {
-    dataState: DATA_STATES.loading,
-    showCartModal: false,
-    products: [],
-    cart: []
-  };
+export default function App() {
+  const [dataState, setDataState] = useState(DATA_STATES.loading);
+  const [showCartModal, setCartModalVisibility] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
-  componentDidMount() {
-    this.fetchProducts(API_URL).then(products =>
-      this.setState({ products, dataState: DATA_STATES.loaded })
-    );
-  }
+  useEffect(() => {
+    fetchProducts(API_URL).then(products => {
+      setProducts(products);
+      setDataState(DATA_STATES.loaded);
+    });
+  }, []);
 
-  fetchProducts = async url => {
+  const fetchProducts = async url => {
     const response = await fetch(url);
     const products = await response.json();
     return products;
   };
 
-  toggleCartModal = () => {
-    this.setState(state => {
-      return {
-        showCartModal: !state.showCartModal
-      };
-    });
+  const toggleCartModal = () => {
+    setCartModalVisibility(!showCartModal);
   };
 
-  addToCart = id => {
-    this.setState(state => {
-      const { cart, products } = state;
-      const isProductAlreadyInCart = !!cart.find(
-        productInCart => productInCart.id === id
+  const addToCart = id => {
+    const isProductAlreadyInCart = !!cart.find(
+      productInCart => productInCart.id === id
+    );
+    if (isProductAlreadyInCart) {
+      setCart(
+        cart.map(productInCart => {
+          if (productInCart.id === id) {
+            return { ...productInCart, quantity: productInCart.quantity + 1 };
+          }
+          return productInCart;
+        })
       );
-
-      if (isProductAlreadyInCart) {
-        return {
-          cart: cart.map(productInCart => {
-            if (productInCart.id === id) {
-              return { ...productInCart, quantity: productInCart.quantity + 1 };
-            }
-            return productInCart;
-          })
-        };
-      }
-
+    } else {
       const product = products.find(product => product.id === id);
-      return { cart: [...cart, { ...product, quantity: 1 }] };
-    });
+      setCart([...cart, { ...product, quantity: 1 }]);
+    }
   };
 
-  removeFromCart = id => {
-    this.setState(state => {
-      const { cart } = state;
-      const productInCart = cart.find(productInCart => productInCart.id === id);
+  const removeFromCart = id => {
+    const productInCart = cart.find(productInCart => productInCart.id === id);
 
-      if (productInCart) {
-        if (productInCart.quantity < 2) {
-          return {
-            cart: cart.filter(productInCart => productInCart.id !== id)
-          };
-        }
-
-        return {
-          cart: cart.map(productInCart => {
+    if (productInCart) {
+      if (productInCart.quantity < 2) {
+        setCart(cart.filter(productInCart => productInCart.id !== id));
+      } else {
+        setCart(
+          cart.map(productInCart => {
             if (productInCart.id === id) {
               return { ...productInCart, quantity: productInCart.quantity - 1 };
             }
             return productInCart;
           })
-        };
+        );
       }
-    });
+    }
   };
 
-  render() {
-    const { dataState, products, cart, showCartModal } = this.state;
-
-    return (
-      <>
-        <TopBar cart={cart} toggleCartModal={this.toggleCartModal} />
-        <Cart
-          showCartModal={showCartModal}
-          toggleCartModal={this.toggleCartModal}
-          cart={cart}
-          addToCart={this.addToCart}
-          removeFromCart={this.removeFromCart}
-        />
-        <Container>
-          <Row style={{marginTop: 70}}>
-            <Col>
-              <Products
-                dataState={dataState}
-                products={products}
-                cart={cart}
-                addToCart={this.addToCart}
-              />
-            </Col>
-          </Row>
-        </Container>
-      </>
-    );
-  }
+  return (
+    <>
+      <TopBar cart={cart} toggleCartModal={toggleCartModal} />
+      <Cart
+        showCartModal={showCartModal}
+        toggleCartModal={toggleCartModal}
+        cart={cart}
+        addToCart={addToCart}
+        removeFromCart={removeFromCart}
+      />
+      <Container>
+        <Row style={{ marginTop: 70 }}>
+          <Col>
+            <Products
+              dataState={dataState}
+              products={products}
+              cart={cart}
+              addToCart={addToCart}
+            />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
